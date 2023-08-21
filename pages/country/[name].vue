8cd027fdedd36daa053236e15aa202e4febc9bd5
Navid <nahardaninavid1993@gmail.com>
2023-08-22 02:09:42 +0330

    adding flags into homepage and fetch from api

---
+++ pages/country/[name].vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        hello
+        {{ route.params.name }}
+    </div>
+</template>
+<script setup>
+const route = useRoute()
+console.log(route)
+</script>
---
+++ pages/country/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        This is country page
+        {{ coutryDetail[0].borders }}
+    </div>
+</template>
+<script setup>
+const route = useRoute()
+const name = route.query.name
+const { data : coutryDetail } = await useFetch(`https://restcountries.com/v3.1/name/${name}`)
+</script>
